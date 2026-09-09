55b5c299c197ed49bcab1ed026a711581aed20f1
feketemarcell2024d-code <fekete.marcell.2024d@telekimezotur.hu>
2026-09-09 12:24:43 +0200

    adat bekérés

--- alapok/Program.cs
+++ alapok/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.WriteLine("Kérem a tanulo nevét: ");
+string TanuloNev = Console.ReadLine();
+Console.WriteLine("Kérem a tanulo pontszamat: ");
+int tanulopontszam = int.Parse(Console.ReadLine());
+int maxpontszam = 97;
+if 
