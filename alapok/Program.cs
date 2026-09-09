f7ff52532ec53a16fdf25f7afc4a5014663f0e7f
feketemarcell2024d-code <fekete.marcell.2024d@telekimezotur.hu>
2026-09-09 12:57:41 +0200

    kész

--- alapok/Program.cs
+++ alapok/Program.cs
@@ -7,4 +7,8 @@ double szazalek = (double)tanulopontszam / maxpontszam * 100;
 //szazalek = Math.Round(szazalek, 0);
 Console.WriteLine($"A tanulo neve: {TanuloNev}");
 Console.WriteLine($"Eredmény: @{tanulopontszam}/{maxpontszam} ({szazalek:F2}%)");
+if (szazalek >= 50)
+    Console.WriteLine("A tanulo eredménye: Sikeres");
 
+else
+    Console.WriteLine("A tanulo eredménye: Sikertelen!");
