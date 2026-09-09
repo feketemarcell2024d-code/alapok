f75ea1c2effce172ca550a7f74689a7eaa3fbba9
feketemarcell2024d-code <fekete.marcell.2024d@telekimezotur.hu>
2026-09-09 12:52:46 +0200

    aaa

--- alapok/Program.cs
+++ alapok/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Kérem a tanulo pontszamat: ");
 int tanulopontszam = int.Parse(Console.ReadLine());
 int maxpontszam = 97;
 double szazalek = (double)tanulopontszam / maxpontszam * 100;
-szazalek = Math.Round(szazalek, 0);
+//szazalek = Math.Round(szazalek, 0);
 Console.WriteLine($"A tanulo neve: {TanuloNev}");
 Console.WriteLine($"Eredmény: @{tanulopontszam}/{maxpontszam} ({szazalek:F2}%)");
 
